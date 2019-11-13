c80b2fb577fc0d928a3797c9cd9d9aa59454f552
DevilD <sipuna.biswal@gmail.com>
2019-11-13 18:59:21 +0530

    Sample String operators
    Sample String operators

---
+++ StringOperations/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char cChar = 'A';
+            string sString = "a";
+            Console.WriteLine(Convert.ToInt32(cChar));
+            Console.WriteLine(Convert.ToInt32(Convert.ToChar(sString)));
+            Console.ReadKey();
+        }
+    }
+}
